c2df298e6ee802669c7820adbc7a765f8a749914
Sohel Shekh <contact.sohelshekh@gmail.com>
2022-05-23 11:24:06 +0530

    Admission Enquiry Resposivness Done

--- pages/admission-enquiry.js
+++ pages/admission-enquiry.js
@@ -1,13 +1,13 @@
 export default function AdmissionEnquiry() {
   return (
     <div className="page-layout">
-      <div className="flex my-16 lg:my-32 px-20">
-        <div className="w-1/2">
+      <div className="flex flex-col-reverse md:flex-row my-16 lg:my-32 px-20 xs:px-5">
+        <div className="w-100 md:w-1/2 sm:mt-14">
           <div className="flex flex-col">
             <h1 className="text-heading ">Admission Proccess</h1>
             <span className="underline w-2/12"></span>
           </div>
-          <div className="flex flex-col mt-14 font-semibold">
+          <div className="flex flex-col mt-14 font-semibold debug">
             <p className="py-2">
               1. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Saepe,
@@ -55,16 +55,16 @@ export default function AdmissionEnquiry() {
             aliquid.
           </div>
         </div>
-        <div className="divider divider-horizontal"></div>
-        <div className="w-1/2">
+        <div className="divider md:divider-horizontal divider-vertical"></div>
+        <div className="w-100 md:w-1/2 md:mt-14">
           <div className="flex flex-col">
             <h1 className="text-heading ">Admission Enquiry</h1>
             <span className="underline w-2/12"></span>
           </div>
-          <dir className="mt-14">
+          <dir className="sm:mt-10 xs:pl-0">
             <form>
-              <div className="flex">
-                <div className="w-1/2">
+              <div className="flex flex-col sm:flex-row">
+                <div className="w-1/2 xs:w-full">
                   <div className="form-control py-2 pt-0 w-full max-w-xs">
                     <label className="label">
                       <span className="label-text">Student Name</span>
@@ -90,7 +90,7 @@ export default function AdmissionEnquiry() {
                       <span className="label-text">School</span>
                     </label>
                     <select className="select select-bordered">
-                      <option disabled selected>
+                      <option disabled value="DEFAULT">
                         Select School
                       </option>
                       <option>Bharatiya Vidya Bhavans</option>
@@ -116,6 +116,8 @@ export default function AdmissionEnquiry() {
                     </label>
                     <input
                       type="number"
+                      minLength="10"
+                      maxLength="10"
                       placeholder="Mobile Number"
                       className="input input-bordered w-full max-w-xs font-semibold"
                     />
@@ -132,43 +134,45 @@ export default function AdmissionEnquiry() {
                     />
                   </div>
                 </div>
-                <div className="w-1/2 pl-2">
-                  <div className="form-control py-2 pt-0 w-full max-w-xs">
-                    <label className="label">
-                      <span className="label-text">Current Standard</span>
-                    </label>
-                    <select className="select select-bordered">
-                      <option disabled selected>
-                        Select your current standard
-                      </option>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                      <option>6</option>
-                      <option>7</option>
-                      <option>8</option>
-                      <option>9</option>
-                      <option>10</option>
-                      <option>11 Maths</option>
-                      <option>11 Bio</option>
-                      <option>12 Maths</option>
-                      <option>12 Bio</option>
-                    </select>
-                  </div>
-                  <div className="form-control py-2 w-full max-w-xs">
-                    <label className="label">
-                      <span className="label-text">BOARD</span>
-                    </label>
-                    <select className="select select-bordered">
-                      <option disabled selected>
-                        Select your school BOARD
-                      </option>
-                      <option>CBSE</option>
-                      <option>ICSE</option>
-                      <option>GSEB</option>
-                    </select>
+                <div className="w-1/2 pl-2 xs:pl-0 xs:w-100">
+                  <div className="flex flex-row sm:flex-col">
+                    <div className="form-control py-2 pt-0 w-10/12 sm:w-full max-w-xs">
+                      <label className="label">
+                        <span className="label-text">Current Standard</span>
+                      </label>
+                      <select className="select select-bordered">
+                        <option disabled value="DEFAULT">
+                          Select your current standard
+                        </option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+                        <option>9</option>
+                        <option>10</option>
+                        <option>11 Maths</option>
+                        <option>11 Bio</option>
+                        <option>12 Maths</option>
+                        <option>12 Bio</option>
+                      </select>
+                    </div>
+                    <div className="form-control py-0 pl-2 sm:pl-0 sm:py-2 w-full max-w-xs">
+                      <label className="label">
+                        <span className="label-text">BOARD</span>
+                      </label>
+                      <select className="select select-bordered">
+                        <option disabled value="DEFAULT">
+                          Select your school BOARD
+                        </option>
+                        <option>CBSE</option>
+                        <option>ICSE</option>
+                        <option>GSEB</option>
+                      </select>
+                    </div>
                   </div>
                   <div className="form-control py-2 w-full max-w-xs">
                     <label className="label">
